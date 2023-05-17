4cccff6271241e898e554b92f09240dbe5a42cb9
Alexandre Marcelo <contact@xandecelo.com>
2023-05-17 17:05:53 -0300

    Added example file stub

---
+++ padlcore/scripts/padlbridge.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+export PADLBRIDGE_HOME=/opt/padlbridge
+export CONFIG_FILE=$1
+export UPDATE_DELAY_SECS=30
+export PADL_RUN_FLAG=true
+export SLEEP_PID=
+
+trap end_padl_app INT TERM
+
+end_padl_app() {
+    echo "Ending padlbridge app..."
+    PADL_RUN_FLAG=false
+    if [ ! -z $SLEEP_PID ]
+    then
+        kill $SLEEP_PID
+    fi
+}
+
+check_config_file() {
+
+    echo "Checking configuration file location."
+    if [ -z ${PADL_CONFIG_FILE} ]
+    then
+        echo "Variable PADL_CONFIG_FILE is not set."
+    else
+        CONFIG_FILE="${PADL_CONFIG_FILE}"
+        echo "Using configuration file provided by PADL_CONFIG_FILE variable."
+    fi
+
+    if [ -z ${CONFIG_FILE} ]
+    then
+        CONFIG_FILE="${PADLBRIDGE_HOME}/conf/padlbridge.yaml"
+        echo "No configuration file specified. Using configuration file default location."
+    else 
+        echo "Configuration file parameter provided."
+    fi
+
+}
+run_padl_action() {
+    local action=$1
+    ${PADLBRIDGE_HOME}/core/bin/core $action ${CONFIG_FILE} 2>&1 1> >(tee) 
+}
+
+check_config_file
+echo "Using configuration file located at ${CONFIG_FILE}."
+while $PADL_RUN_FLAG
+    run_padl_action run
+    sleep $UPDATE_DELAY_SECS &
+    SLEEP_PID=$$
+    wait $SLEEP_PID
+    SLEEP_PID=
+done
