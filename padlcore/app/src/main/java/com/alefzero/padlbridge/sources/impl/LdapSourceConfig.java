d3406c5572c5163fee39895b077ab95f8d5e563b
Alexandre Marcelo <contact@xandecelo.com>
2023-05-17 03:37:22 -0300

    LdapSource implementation

--- padlcore/app/src/main/java/com/alefzero/padlbridge/sources/impl/LdapSourceConfig.java
+++ padlcore/app/src/main/java/com/alefzero/padlbridge/sources/impl/LdapSourceConfig.java
@@ -5,6 +5,10 @@ import com.alefzero.padlbridge.config.model.SourceConfig;
 public class LdapSourceConfig extends SourceConfig {
 	private String host;
 	private int port;
+	private String username;
+	private String password;
+	private String useTLS;
+	private String baseDN;
 
 	public String getHost() {
 		return host;
@@ -22,10 +26,42 @@ public class LdapSourceConfig extends SourceConfig {
 		this.port = port;
 	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUseTLS() {
+		return useTLS;
+	}
+
+	public void setUseTLS(String useTLS) {
+		this.useTLS = useTLS;
+	}
+
+	public String getBaseDN() {
+		return baseDN;
+	}
+
+	public void setBaseDN(String baseDN) {
+		this.baseDN = baseDN;
+	}
+
 	@Override
 	public String toString() {
-		return "LdapSourceConfig [host=" + host + ", port=" + port + ", getType()=" + getType() + ", getName()="
-				+ getName() + "]";
+		return "LdapSourceConfig [host=" + host + ", port=" + port + ", username=" + username + ", password=" + password
+				+ ", useTLS=" + useTLS + ", baseDN=" + baseDN + "]";
 	}
 
 }
--- padlcore/app/src/main/java/com/alefzero/padlbridge/sources/impl/LdapSourceService.java
+++ padlcore/app/src/main/java/com/alefzero/padlbridge/sources/impl/LdapSourceService.java
@@ -1,33 +1,89 @@
 package com.alefzero.padlbridge.sources.impl;
 
+import java.sql.SQLException;
+import java.util.ArrayDeque;
+import java.util.Base64.Decoder;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.Deque;
 import java.util.Iterator;
 
 import com.alefzero.padlbridge.core.model.DataEntry;
+import com.alefzero.padlbridge.exceptions.PadlUnrecoverableError;
 import com.alefzero.padlbridge.sources.PBSourceService;
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPConnectionPool;
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.SearchResult;
+import com.unboundid.ldap.sdk.SearchResultEntry;
+import com.unboundid.ldap.sdk.SearchScope;
 
 public class LdapSourceService extends PBSourceService {
 
+	private LDAPConnectionPool pool = null;
+	private LdapSourceConfig config;
+
 	@Override
-	public String toString() {
-		return "LdapService [toString()=" + super.toString() + "]";
+	public void prepare() {
+		config = (LdapSourceConfig) super.getConfig();
+		try {
+			LDAPConnection conn = new LDAPConnection(config.getHost(), config.getPort(), config.getUsername(),
+					config.getPassword());
+			pool = new LDAPConnectionPool(conn, 10);
+		} catch (LDAPException e) {
+			logger.error("Error creating target LDAP connections.", e);
+		}
 	}
 
 	@Override
 	public Iterator<DataEntry> getAllEntries() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void prepare() {
-		// TODO Auto-generated method stub
-		
+		try (LDAPConnection conn = pool.getConnection()) {
+			SearchResult result = conn.search(config.getBaseDN(), SearchScope.SUBORDINATE_SUBTREE, "(objectClass=*)",
+					"*");
+			return new EntryIterator(result);
+		} catch (LDAPException e) {
+			e.printStackTrace();
+			throw new PadlUnrecoverableError(e);
+		}
 	}
 
 	@Override
 	public Iterator<String> getAllUids() {
-		// TODO Auto-generated method stub
-		return null;
+		Deque<String> dnItems = new ArrayDeque<String>();
+		try (LDAPConnection conn = pool.getConnection()) {
+			SearchResult result = conn.search(config.getBaseDN(), SearchScope.SUBORDINATE_SUBTREE, "(objectClass=*)",
+					"dn");
+			result.getSearchEntries().forEach(entry -> {
+				dnItems.add(entry.getDN());
+			});
+
+		} catch (LDAPException e) {
+			// TODO: throw exception and treat.
+			e.printStackTrace();
+		}
+		return dnItems.iterator();
 	}
 
+	class EntryIterator implements Iterator<DataEntry> {
+
+		private Iterator<SearchResultEntry> iterator;
+
+		private EntryIterator(SearchResult result) {
+			this.iterator = result.getSearchEntries().iterator();
+		}
+
+		@Override
+		public boolean hasNext() {
+			return iterator.hasNext();
+		}
+
+		@Override
+		public DataEntry next() {
+			SearchResultEntry entry = iterator.next();
+			DataEntry dataEntry = new DataEntry(entry.getDN(), entry, DigestUtils.md5Hex(entry.toLDIFString()));
+			return dataEntry;
+		}
+
+	}
 }
